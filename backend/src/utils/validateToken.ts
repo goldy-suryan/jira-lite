501b8e6f310d5946fb70f53a0ce75d875f9cf82f
Goldy Suryan <goldy.suryan@builder.ai>
2026-03-20 16:54:35 +0530

    fix: handle error while validating token

--- backend/src/utils/validateToken.ts
+++ backend/src/utils/validateToken.ts
@@ -2,9 +2,15 @@ import jwt from 'jsonwebtoken';
 
 export function authenticateToken(token: string) {
   const secret = process.env.JWT_SECRET as string;
-  if (token) {
+  if (!token) return null;
+
+  try {
     const user = jwt.verify(token, secret);
     return user;
+  } catch (err: any) {
+    if (err.name === 'TokenExpiredError') {
+      return null;
+    }
+    throw err;
   }
-  return null;
 }
